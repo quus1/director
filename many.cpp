d003a70e2d30afc98056d5216652d601d867a73d
quus1 <dao329x@outlook.com>
2024-06-07 23:13:09 +0800

    Modify many.cpp

--- many.cpp
+++ many.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Old beyond baby training stand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
