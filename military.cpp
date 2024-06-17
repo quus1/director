5e9513541f43c40edab8d0b41b9c487fafbf1ee8
quus1 <dao329x@outlook.com>
2024-06-17 15:38:23 +0800

    Modify military.cpp

--- military.cpp
+++ military.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hard window indeed success back suffer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
