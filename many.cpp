0fe2943b4584673974b8cb457413cf13da7d4f1e
quus1 <dao329x@outlook.com>
2024-06-20 23:46:26 +0800

    Modify many.cpp

--- many.cpp
+++ many.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Forget state prove offer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
