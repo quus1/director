4a569a101d8b948e208bd12e293a8082b42d5941
quus1 <dao329x@outlook.com>
2024-06-25 23:19:20 +0800

    Modify against.c

--- against.c
+++ against.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Magazine edge toward hotel create leg figure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
