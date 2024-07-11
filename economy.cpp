e97978cb2e4ab859d7993908f6a63b79c750be3a
quus1 <dao329x@outlook.com>
2024-07-11 18:08:07 +0800

    Modify economy.cpp

--- economy.cpp
+++ economy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sit stage probably knowledge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
