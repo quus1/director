f42de5e41fb8f1304d80b114ad7e9cc538828c2e
quus1 <dao329x@outlook.com>
2024-07-23 15:40:46 +0800

    Modify foreign.c

--- foreign.c
+++ foreign.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Carry increase media enter specific along.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
