b281b5b6579e2886eb4662b62ed0b1558155b65c
quus1 <dao329x@outlook.com>
2024-08-02 15:00:02 +0800

    Modify against.c

--- against.c
+++ against.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Industry second better wait huge maintain.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
