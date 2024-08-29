dcfec38ccd6d9eecbd5d3441d7e56a13cd229198
quus1 <dao329x@outlook.com>
2024-08-29 14:18:20 +0800

    Modify building.c

--- building.c
+++ building.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Within natural decision policy listen commercial.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
