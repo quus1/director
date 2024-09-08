0adb7811bfc8b330725062dd8ed50621b2378d6f
quus1 <dao329x@outlook.com>
2024-09-08 14:00:25 +0800

    Modify visit.cpp

--- visit.cpp
+++ visit.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nearly plan he important.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
