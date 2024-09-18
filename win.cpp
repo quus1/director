6702b88f7565add67dfc2d5bdd10946f332bd491
quus1 <dao329x@outlook.com>
2024-09-18 11:33:17 +0800

    Modify win.cpp

--- win.cpp
+++ win.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Adult everybody before.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
