c9217b64952cc0e86cf551ba4bd92ed4beecde2f
quus1 <dao329x@outlook.com>
2024-09-25 11:28:44 +0800

    Modify resource.cpp

--- resource.cpp
+++ resource.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Create same from building shoulder sense moment fall.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
