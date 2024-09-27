e7e5c6ce86090887ef0c2dbe3f288097af7b9da4
quus1 <dao329x@outlook.com>
2024-09-27 12:44:26 +0800

    Modify possible.cpp

--- possible.cpp
+++ possible.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Look firm the security follow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
