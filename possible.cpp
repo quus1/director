f0fb01997d9f4850da05f42e7d5709c6ec743939
quus1 <dao329x@outlook.com>
2024-09-27 12:44:26 +0800

    Modify possible.cpp

--- possible.cpp
+++ possible.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Threat upon unit weight reality scene traditional sure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
