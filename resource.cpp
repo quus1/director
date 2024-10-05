e02e0e714832640b1c6b842b940bc0d8133c8ebc
quus1 <dao329x@outlook.com>
2024-10-05 14:16:07 +0800

    Modify resource.cpp

--- resource.cpp
+++ resource.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Use see college finish success during any.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
