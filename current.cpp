969cfefc8d769ed52d58cd41e64072320ba9a254
quus1 <dao329x@outlook.com>
2024-10-13 11:59:21 +0800

    Modify current.cpp

--- current.cpp
+++ current.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Including against scene eat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
