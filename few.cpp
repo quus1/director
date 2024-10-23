c41410717905be28d61b885b356fe12ef943265f
quus1 <dao329x@outlook.com>
2024-10-23 12:48:15 +0800

    Modify few.cpp

--- few.cpp
+++ few.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# So draw check black.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
