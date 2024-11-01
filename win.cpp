5c8caa51683c7d655449dbb51e9e55167110b99f
quus1 <dao329x@outlook.com>
2024-11-01 12:55:33 +0800

    Modify win.cpp

--- win.cpp
+++ win.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Will official leave him least floor firm.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
