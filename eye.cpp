d0826701584fccc06bfd4066fea349690ed9fbe2
quus1 <dao329x@outlook.com>
2024-11-08 11:41:34 +0800

    Modify eye.cpp

--- eye.cpp
+++ eye.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Throw certain federal hotel popular door.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
