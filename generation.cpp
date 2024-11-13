060cb3829d87ed9503a2e33e488de82f4154fd7a
quus1 <dao329x@outlook.com>
2024-11-13 14:41:10 +0800

    Modify generation.cpp

--- generation.cpp
+++ generation.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Appear area clear benefit those.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
