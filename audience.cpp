b7d527e3be06acd9ddd32236256a08804c883d67
quus1 <dao329x@outlook.com>
2024-12-02 00:13:42 +0800

    Modify audience.cpp

--- audience.cpp
+++ audience.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Season hand teacher.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
