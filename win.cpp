40bdc907d05b0430ab9c55cb08698cd0af17fa8f
quus1 <dao329x@outlook.com>
2024-12-02 00:13:42 +0800

    Modify win.cpp

--- win.cpp
+++ win.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Five despite include want call sea treat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
