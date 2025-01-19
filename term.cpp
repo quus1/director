1411678ec524b578ce8eea69c3b0e4cc59224877
quus1 <dao329x@outlook.com>
2025-01-19 14:20:39 +0800

    Modify term.cpp

--- term.cpp
+++ term.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# From collection quickly job still around.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
