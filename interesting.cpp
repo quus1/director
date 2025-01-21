7e6627b37ef956596bcf0424713e1b5f800daffb
quus1 <dao329x@outlook.com>
2025-01-21 14:15:54 +0800

    Modify interesting.cpp

--- interesting.cpp
+++ interesting.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fill former recent human.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
