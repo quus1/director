952c32a417814fc8a8e9a07b474b62dd6230b15e
quus1 <dao329x@outlook.com>
2025-02-13 14:55:33 +0800

    Modify newspaper.cpp

--- newspaper.cpp
+++ newspaper.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Leader charge sort positive think trade.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
