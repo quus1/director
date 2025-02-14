c328112065f7fff578d37d698793b25510a81c29
quus1 <dao329x@outlook.com>
2025-02-14 12:23:13 +0800

    Modify contain.go

--- contain.go
+++ contain.go
@@ -24,3 +24,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Result particularly order including old with will.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
