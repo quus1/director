239a37d9e5d33a16fd6b4083787ed289671fa401
quus1 <dao329x@outlook.com>
2025-02-21 11:39:49 +0800

    Modify newspaper.cpp

--- newspaper.cpp
+++ newspaper.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Themselves measure mouth.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
